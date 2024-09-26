92066f22548f07fd261faa6b32065f9f8f55e245
victorndungi <victor.ndungi@student.moringaschool.com>
2024-09-26 20:08:49 +0300

    Completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.sorted_word = ''.join(sorted(word))
+    
+    def match(self, possible_anagarams):
+        return [anagram for anagram in possible_anagarams if ''.join(sorted(anagram)) == self.sorted_word]
